a20af30521129984f2b824fb121d4c7ae7973abb
katizab <100358952+katizab@users.noreply.github.com>
2024-10-17 01:08:05 +0000

    4

--- env_test.rb
+++ env_test.rb
@@ -2,7 +2,7 @@ require "http"
 require "json"
 require "dotenv/load"
 
-pirate_weather_api_key = ENV["PIRATE_WEATHER_API_KEY"]
+pirate_weather_api_key = ENV["PIRATE_WEATHER_KEY"]
 gmaps_api_key=ENV["GMAPS_KEY"]
 openai_api_key=ENV["OPENAI_KEY"]
 
@@ -10,23 +10,23 @@ pp "Where are you?"
 location=gets.chomp
 
 
-
+# Get the user’s latitude and longitude from the Google Maps API.
 google_maps_url= "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{gmaps_api_key}"
 raw_response_maps = HTTP.get(google_maps_url)
 parsed_response_maps = JSON.parse(raw_response_maps)
 
-pp parsed_response_maps.keys
+#pp parsed_response_maps.keys
 
 latitude = parsed_response_maps.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lat")
 longitude = parsed_response_maps.fetch("results").at(0).fetch("geometry").fetch("location").fetch("lng")
-pp latitude
-pp longitude
-#pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/#{latitude},#{longitude}"
 
-#x = HTTP.get(pirate_weather_url)
-
-# Get the user’s latitude and longitude from the Google Maps API.
 
 # Get the weather at the user’s coordinates from the Pirate Weather API.
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{latitude},#{longitude}"
+raw_response_weather = HTTP.get(pirate_weather_url)
+parsed_response_weather = JSON.parse(raw_response_weather)
+
+current_temp=parsed_response_weather.fetch("currently").fetch("temperature")
 
 # Display the current temperature and summary of the weather for the next hour.
+puts "It is currently #{current_temp}°F."
